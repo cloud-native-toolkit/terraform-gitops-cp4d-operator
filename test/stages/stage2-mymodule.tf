952fd9927271117ebb635cb59f9249877a74f3ad
Andrew Trice <amtrice@us.ibm.com>
2022-11-08 12:37:11 -0500

    added cp_catalogs to test case
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,5 +1,8 @@
 module "cp4d_operator" {
   source = "./module"
+  depends_on = [
+    module.cp_catalogs
+  ]
 
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
